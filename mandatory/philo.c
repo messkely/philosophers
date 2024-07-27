dd852860b2603d6453c059841f46ac8d9ff87455
Mohamed Esskely <messkely@e3r2p10.1337.ma>
2024-07-27 18:16:26 +0100

    dd

--- mandatory/philo.c
+++ mandatory/philo.c
@@ -6,7 +6,7 @@
 /*   By: messkely <messkely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 10:03:55 by messkely          #+#    #+#             */
-/*   Updated: 2024/05/27 15:36:37 by messkely         ###   ########.fr       */
+/*   Updated: 2024/07/27 11:58:38 by messkely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int	main(int ac, char **av)
 		return (write(2, "Wrong argument count\n", 22), 1);
 	check_args_is_valid(av);
 	init_table(&philos);
+	philos_procces(&philos);
 	return (0);
 }
